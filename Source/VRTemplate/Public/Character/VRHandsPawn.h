08cfdd41743693cd65bff3339eefec6d47b67ee2
arcrofts2000 <arcrofts2000@gmail.com>
2024-02-20 17:40:50 +0000

    Added some basic classes.

---
+++ Source/VRTemplate/Public/Character/VRHandsPawn.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Character/VRBasePawn.h"
+#include "VRHandsPawn.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class VRTEMPLATE_API AVRHandsPawn : public AVRBasePawn
+{
+	GENERATED_BODY()
+	
+};
